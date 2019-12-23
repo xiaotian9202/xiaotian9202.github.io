e5f440789ccc791f39e485a382c0358e5ec89ca5
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-23 20:34:43 +0800

    [yanxiaotian] {fix}-{Chapter3-Story3}: Adjust structure of code

--- Homeworks/chapter3/story3/script/script.js
+++ Homeworks/chapter3/story3/script/script.js
@@ -42,38 +42,70 @@ var carProducts = [
     "checked": false
   }
 ]
+var tbody = document.querySelector("tbody");
+var table = document.querySelector("table");
+
+for (var i = 0, len = carProducts.length; i < len; i++) {
+  tbody.innerHTML += "<tr class='item"+ carProducts[i].id + "'></tr>";
+  var name = ".item" + carProducts[i].id;
+  var tr = document.querySelector(name);
+  tr.innerHTML = "<td>"
+  + "<input class='check' type='checkbox'" + getType(carProducts[i].checked) + " / >" 
+  + "</td>" 
+  + "<td>" + carProducts[i].name + "</td>" 
+  + "<td class='price'>" + carProducts[i].price + "</td>"
+  + "<td>" 
+  + "<input class='plus-minus btn-style' type='button' value='-'/ >"
+  + "<span class='nums'>" + carProducts[i].count + "</span>"
+  + "<input class='plus-minus btn-style' type='button' value='+'/ >"
+  + "</td>"
+  + "<td class='total'>" + carProducts[i].price * carProducts[i].count +"</td>";
+}
+tbody.innerHTML += "<td><span>全选</span>"
+  + "<input class='checkAll' type='checkbox'/ >"
+  + "</td>"
+  + "<td class='message' colspan='4'>共计" 
+  + "<span class='totalNums'>3</span>件商品，"
+  + "<span class='totalMoney'>210</span>￥" 
+  + "</td>";
+
+  table.addEventListener("click", function(e) {
+    if (e.target.className === "check") {
+      isCheckAll();
+      changeMessage();
+    }
+  
+    if (e.target.className === "checkAll") {
+      var checks = document.querySelectorAll(".check");
+      for (var i = 0, lens = checks.length; i < lens; i++) {
+        checks[i].checked = e.target.checked;
+      }
+      changeMessage();
+    }
+  
+    if (e.target.className === "plus-minus btn-style") {
+      var nodeOfTr = e.target.parentNode.parentNode;
+      if (e.target.value === "-") {
+        var itemNum = parseInt(e.target.nextElementSibling.innerHTML) - 1;
+        if (itemNum <= 0) {
+          nodeOfTr.parentNode.removeChild(nodeOfTr);
+        } else {
+          e.target.nextElementSibling.innerHTML = itemNum;
+        }
+      } else if (e.target.value === "+") {
+        var itemNum = parseInt(e.target.previousElementSibling.innerHTML) + 1;
+        e.target.previousElementSibling.innerHTML = itemNum;
+      }
+      var itemPrice = parseFloat(nodeOfTr.querySelector(".price").innerHTML);
+      var totalPrice = getTotalPrice(itemNum, itemPrice);
+      nodeOfTr.querySelector(".total").innerHTML = totalPrice;
+      changeMessage();
+    }
+  })
 
 function getType(val) {
   return val ? "checked" : "";
 }
-
-var tbody = document.querySelector("tbody");
-for (var i = 0, len = carProducts.length; i < len; i++) {
-  tbody.innerHTML += "<tr id=\"item"+ carProducts[i].id + "\"></tr>";
-  var id = "#item" + carProducts[i].id;
-  var tr = document.querySelector(id);
-  tr.innerHTML = "<td>"
-  + "<input class=\"check\" type=\"checkbox\"" + getType(carProducts[i].checked) + " / >" 
-  + "</td>" 
-  + "<td>" + carProducts[i].name + "</td>" 
-  + "<td class=\"price\">" + carProducts[i].price + "</td>"
-  + "<td>" 
-  + "<input class=\"plus-minus btn-style\" type=\"button\" value=\"-\" />"
-  + "<span class=\"nums\">" + carProducts[i].count + "</span>"
-  + "<input class=\"plus-minus btn-style\" type=\"button\" value=\"+\" />"
-  + "</td>"
-  + "<td class=\"total\">" + carProducts[i].price * carProducts[i].count +"</td>";
-}
-tbody.innerHTML += "<td><span>全选</span>"
-  + "<input class=\"checkAll\" type=\"checkbox\"/>"
-  + "</td>"
-  + "<td></td>"
-  + "<td></td>"
-  + "<td></td>"
-  + "<td class=\"message\">共计" 
-  + "<span class=\"totalNums\">3</span>件商品，"
-  + "<span class=\"totalMoney\">210</span>￥" 
-  + "</td>";
 
 // get total money of pre items
 function getTotalPrice(itemCount, itemPrice) {
@@ -84,6 +116,7 @@ function getTotalPrice(itemCount, itemPrice) {
 // get the message of items numbers and total money
 function changeMessage() {
   var items = document.querySelectorAll(".check");
+  var message = document.querySelector(".message");
   var totalMoney = 0;
   var totalItems = 0;
   for (var i = 0, lens = items.length; i < lens; i++) {
@@ -92,7 +125,6 @@ function changeMessage() {
       totalItems += parseInt(items[i].parentNode.parentNode.querySelector(".nums").innerHTML) 
     }
   }
-  var message = document.querySelector(".message");
   message.innerHTML = "共计" + totalItems + "件商品," + totalMoney + "￥";
 }
 
@@ -107,38 +139,3 @@ function isCheckAll() {
   }
   checkAll.checked = flag;
 }
-
-var table = document.querySelector("table");
-table.addEventListener("click", function(e) {
-  if (e.target.className === "check") {
-    isCheckAll();
-    changeMessage();
-  }
-
-  if (e.target.className === "checkAll") {
-    var checks = document.querySelectorAll(".check");
-    for (var i = 0, lens = checks.length; i < lens; i++) {
-      checks[i].checked = e.target.checked;
-    }
-    changeMessage();
-  }
-
-  if (e.target.className === "plus-minus btn-style") {
-    var nodeOfTr = e.target.parentNode.parentNode;
-    if (e.target.value === "-") {
-      var itemNum = parseInt(e.target.nextElementSibling.innerHTML) - 1;
-      if (itemNum <= 0) {
-        nodeOfTr.parentNode.removeChild(nodeOfTr);
-      } else {
-        e.target.nextElementSibling.innerHTML = itemNum;
-      }
-    } else if (e.target.value === "+") {
-      var itemNum = parseInt(e.target.previousElementSibling.innerHTML) + 1;
-      e.target.previousElementSibling.innerHTML = itemNum;
-    }
-    var itemPrice = parseFloat(nodeOfTr.querySelector(".price").innerHTML);
-    var totalPrice = getTotalPrice(itemNum, itemPrice);
-    nodeOfTr.querySelector(".total").innerHTML = totalPrice;
-    changeMessage();
-  }
-})
