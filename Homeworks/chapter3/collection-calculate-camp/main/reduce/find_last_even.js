882250e043bf5e7d1426f5415b9d347756f47f27
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 22:38:59 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of find_last_even.js

--- Homeworks/chapter3/collection-calculate-camp/main/reduce/find_last_even.js
+++ Homeworks/chapter3/collection-calculate-camp/main/reduce/find_last_even.js
@@ -2,6 +2,12 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  var lens = collection.length;
+  for (var i = lens - 1; i >= 0; i--) {
+    if (!(collection[i] & 1)) {
+      return collection[i];
+    }
+  }
 }
 
 module.exports = find_last_even;
