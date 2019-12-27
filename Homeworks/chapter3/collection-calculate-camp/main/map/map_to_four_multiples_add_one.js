f132c9606cc9895df1924475569b896c23926d31
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 19:10:15 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of map_to_four_multiples_add_one.js

--- Homeworks/chapter3/collection-calculate-camp/main/map/map_to_four_multiples_add_one.js
+++ Homeworks/chapter3/collection-calculate-camp/main/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,9 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    collection[i] = collection[i] * 4 + 1;
+  }
+  return collection;
 };
 
 module.exports = map_to_four_multiples_add_one;
