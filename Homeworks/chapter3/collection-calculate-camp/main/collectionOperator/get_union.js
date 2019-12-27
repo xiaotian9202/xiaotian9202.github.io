bf70bac81dd3c1c1d39073c393859623a43848b6
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 12:51:12 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of get_union.js

--- Homeworks/chapter3/collection-calculate-camp/main/collectionOperator/get_union.js
+++ Homeworks/chapter3/collection-calculate-camp/main/collectionOperator/get_union.js
@@ -2,6 +2,19 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  for (var i = 0, Blens = collection_b.length; i < Blens; i++) {
+    var flag = false;
+    for (var j = 0, Alens = collection_a.length; j < Alens; j++) {
+      if (collection_a[j] === collection_b[i]) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      collection_a.push(collection_b[i]);
+    }
+  }
+  return collection_a;
 }
 
 module.exports = get_union;
