5229d23846121b8ea5f48d52bd8d335e4a4c2222
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 14:20:14 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of two_collections_practice_2.js

--- Homeworks/chapter3/collection-calculate-camp/main/filter/two_collections_practice_2.js
+++ Homeworks/chapter3/collection-calculate-camp/main/filter/two_collections_practice_2.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
   //在这里写入代码
+  var result = [];
+  var lensA = collection_a.length;
+  var lensB = collection_b.length;
+  for (var i = 0; i < lensA; i++) {
+    var flag = false;
+    for (var j = 0; j < lensB; j++) {
+      if (collection_b[j] === collection_a[i]) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      result.push(collection_a[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = choose_no_common_elements;
