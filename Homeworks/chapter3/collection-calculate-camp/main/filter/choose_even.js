956c5e397226f44ed8a2d85884e4a43093bd0419
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 12:56:54 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of choose-even.js

--- Homeworks/chapter3/collection-calculate-camp/main/filter/choose_even.js
+++ Homeworks/chapter3/collection-calculate-camp/main/filter/choose_even.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_even(collection) {
-
   //在这里写入代码
+  var result = [];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    if (!(collection[i] & 1)) {
+      result.push(collection[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = choose_even;
