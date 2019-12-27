9ff0f306a73c2d9e0e94f3fb001d1ff2f3cb6a0f
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 22:18:02 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of reduce/reduce/compare_collections_spec.js

--- Homeworks/chapter3/collection-calculate-camp/main/reduce/compare_collections.js
+++ Homeworks/chapter3/collection-calculate-camp/main/reduce/compare_collections.js
@@ -3,6 +3,8 @@
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
   var bool = true;
+  collection_a = insertionSort(collection_a);
+  collection_b = insertionSort(collection_b);
   for (var i = 0, lens = collection_a.length; i < lens; i++) {
     if (collection_a[i] !== collection_b[i]) {
       bool = false;
@@ -10,6 +12,19 @@ function compare_collections(collection_a, collection_b) {
     }
   }
   return bool;
+}
+
+function insertionSort(collection) {
+  for (var i = 1, lens = collection.length; i < lens; i++) {
+    var key = collection[i];
+    var j = i - 1;
+    while (j >= 0 && collection[j] < key) {
+      collection[j+1] = collection[j];
+      j--;
+    }
+    collection[j+1] = key;
+  }
+  return collection;
 }
 
 module.exports = compare_collections;
