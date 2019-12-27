ca3c8ef6997879b2da699aa246b83edb2497560a
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 22:32:14 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of find_element_first_subscript.js

--- Homeworks/chapter3/collection-calculate-camp/main/reduce/find_element_first_subscript.js
+++ Homeworks/chapter3/collection-calculate-camp/main/reduce/find_element_first_subscript.js
@@ -2,6 +2,14 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
+  var result = -1;
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    if (collection[i] === element) {
+      result = i;
+      break;
+    }
+  }
+  return result;
 }
 
 module.exports = calculate_elements_sum;
