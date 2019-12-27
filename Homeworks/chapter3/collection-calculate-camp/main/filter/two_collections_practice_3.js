4bea84000fb175b165eabaeede691256efde9288
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 14:37:22 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of two_collections_practice_3.js

--- Homeworks/chapter3/collection-calculate-camp/main/filter/two_collections_practice_3.js
+++ Homeworks/chapter3/collection-calculate-camp/main/filter/two_collections_practice_3.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
   //在这里写入代码
+  var result = [];
+  var lensA = collection_a.length;
+  var lensB = collection_b.length;
+  for (var i = 0; i < lensA; i++) {
+    for (var j = 0; j < lensB; j++) {
+      if (collection_a[i] % collection_b[j] === 0) {
+        result.push(collection_a[i]);
+        break;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = choose_divisible_integer;
