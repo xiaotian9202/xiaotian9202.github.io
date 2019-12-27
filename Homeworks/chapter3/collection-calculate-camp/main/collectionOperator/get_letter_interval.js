d68c7e3319a5e10227cb732fc76ba1c54e1327f2
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 12:37:40 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of get_letter_interval

--- Homeworks/chapter3/collection-calculate-camp/main/collectionOperator/get_letter_interval.js
+++ Homeworks/chapter3/collection-calculate-camp/main/collectionOperator/get_letter_interval.js
@@ -2,6 +2,29 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  var result = [];
+    if (number_a === number_b) {
+      var str = numberToAcode(number_a);
+      result.push(str);
+    } else if (number_a < number_b) {
+      while (number_a <= number_b) {
+        var str = numberToAcode(number_a);
+        result.push(str);
+        number_a++;
+      }
+    } else {
+      while (number_a >= number_b) {
+        var str = numberToAcode(number_a);
+        result.push(str);
+        number_a--;
+      }
+    }
+ 
+  return result;
+}
+
+function numberToAcode(num) {
+  return String.fromCharCode(num + 96);
 }
 
 module.exports = get_letter_interval;
