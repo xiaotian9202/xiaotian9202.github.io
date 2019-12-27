bd66f208b4d8ae7270f992e285b894adbde324ed
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 20:31:58 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of compute_average_spec.js

--- Homeworks/chapter3/collection-calculate-camp/main/reduce/compute_average.js
+++ Homeworks/chapter3/collection-calculate-camp/main/reduce/compute_average.js
@@ -2,6 +2,18 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  var lens = collection.length;
+  var sum = getSum(collection, lens);
+  var average = sum / lens;
+  return average;
+}
+
+function getSum(arr, n) {
+  var sum = 0;
+  for (var i = 0; i < n; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 module.exports = compute_average;
