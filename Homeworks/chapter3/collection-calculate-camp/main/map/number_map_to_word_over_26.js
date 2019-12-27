4faab077693c6c34fc3a1042232ba592229ac94b
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 19:18:28 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of number_map_to_word_over_26.js

--- Homeworks/chapter3/collection-calculate-camp/main/map/number_map_to_word_over_26.js
+++ Homeworks/chapter3/collection-calculate-camp/main/map/number_map_to_word_over_26.js
@@ -1,6 +1,33 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  var result = [];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    var str = getStr(collection[i]);
+    result.push(str)
+  }
+  return result;
 };
+
+function getStr(num) {
+  var table = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z'];
+    var str = "";
+    var result = "";
+  while (num) {
+    var remainder = num % 26;
+    var num = Math.floor(num / 26);
+    if (!remainder) {
+      remainder = 26;
+      num = num - 1;
+    }
+    str += table[remainder];
+  }
+
+  for (var i = str.length-1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
 
 module.exports = number_map_to_word_over_26;
