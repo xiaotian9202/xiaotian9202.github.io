c54ee6ca175f0e4defd70313233767de3dd8bf64
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 14:07:53 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of grouping_count.js

--- Homeworks/chapter3/collection-calculate-camp/main/filter/grouping_count.js
+++ Homeworks/chapter3/collection-calculate-camp/main/filter/grouping_count.js
@@ -1,8 +1,43 @@
 'use strict';
 
 function grouping_count(collection) {
-
   //在这里写入代码
+  var uniqueCollection = choose_no_repeat_number(collection);
+  var result = getCount(uniqueCollection, collection);
+  return result;
+}
+
+function choose_no_repeat_number(collection) {
+  //在这里写入代码
+  var result = [];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    var resultLens = result.length;
+    var flag = false;
+    for (var j = 0; j < resultLens; j++) {
+      if (collection[i] === result[j]) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      result.push(collection[i]);
+    }
+  }
+  return result;
+}
+
+function getCount(uniqueCollection, collection) {
+  var result = {};
+  for (var i = 0, lens = uniqueCollection.length; i < lens; i++) {
+    var count = 0;
+    for (var j = 0, collectionLens = collection.length; j < collectionLens; j++) {
+      if (collection[j] === uniqueCollection[i]) {
+        count++;
+      }
+    }
+    result['' + uniqueCollection[i]] = count;
+  }
+  return result;
 }
 
 module.exports = grouping_count;
