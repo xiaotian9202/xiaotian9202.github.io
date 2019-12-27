d090364df4ec87dc77af93d9dbac1c40d2c9c222
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 22:37:47 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of find_first_even.js

--- Homeworks/chapter3/collection-calculate-camp/main/reduce/find_first_even.js
+++ Homeworks/chapter3/collection-calculate-camp/main/reduce/find_first_even.js
@@ -2,6 +2,12 @@
 
 function find_first_even(collection) {
   //在这里写入代码
+  var lens = collection.length;
+  for (var i = 0; i < lens; i++) {
+    if (!(collection[i] & 1)) {
+      return collection[i];
+    }
+  }
 }
 
 module.exports = find_first_even;
