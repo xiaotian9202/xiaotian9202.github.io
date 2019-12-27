2ef412cf3342f164955d094c1842be374373bc38
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 20:26:11 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of compare_collections_spec.js

--- Homeworks/chapter3/collection-calculate-camp/main/reduce/compare_collections.js
+++ Homeworks/chapter3/collection-calculate-camp/main/reduce/compare_collections.js
@@ -2,6 +2,14 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  var bool = true;
+  for (var i = 0, lens = collection_a.length; i < lens; i++) {
+    if (collection_a[i] !== collection_b[i]) {
+      bool = false;
+      break;
+    }
+  }
+  return bool;
 }
 
 module.exports = compare_collections;
