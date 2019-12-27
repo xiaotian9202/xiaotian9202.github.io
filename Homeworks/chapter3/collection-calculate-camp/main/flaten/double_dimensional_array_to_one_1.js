a5e05ff7e4b9eeae273a7db4e21a821f50f3229a
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 15:08:25 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of double_dimensional_array_to_one_1.JS

--- Homeworks/chapter3/collection-calculate-camp/main/flaten/double_dimensional_array_to_one_1.js
+++ Homeworks/chapter3/collection-calculate-camp/main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  var result = [];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    if (typeof (collection[i]) !== 'number') {
+      for (var j = 0, arrLens = collection[i].length; j < arrLens; j++) {
+        result.push(collection[i][j]);
+      }
+    }
+    else {
+      result.push(collection[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = double_to_one;
