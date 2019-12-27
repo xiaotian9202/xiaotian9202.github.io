3b51f06a83e3f3362ebf36f3c21ea33b13404248
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-27 13:13:28 +0800

    [yanxiaotian] {feat}-{Chapter3-Collection-calculate-camp}: Complete function of choose_no_repeat_number.js

--- Homeworks/chapter3/collection-calculate-camp/main/filter/choose_no_repeat_number.js
+++ Homeworks/chapter3/collection-calculate-camp/main/filter/choose_no_repeat_number.js
@@ -1,8 +1,22 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
   //在这里写入代码
+  var result = [];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    var resultLens = result.length;
+    var flag = false;
+    for (var j = 0; j < resultLens; j++) {
+      if (collection[i] === result[j]) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      result.push(collection[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = choose_no_repeat_number;
