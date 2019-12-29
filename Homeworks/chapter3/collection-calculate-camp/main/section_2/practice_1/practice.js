298bc6bdfc15fc0ea5547c6b2e4ecca38eeeb475
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-29 22:37:42 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of section_2/practice_1/practice.js

--- Homeworks/chapter3/collection-calculate-camp/main/section_2/practice_1/practice.js
+++ Homeworks/chapter3/collection-calculate-camp/main/section_2/practice_1/practice.js
@@ -1,5 +1,45 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var uniqueCollection = choose_no_repeat_number(collection);
+  var result = getCount(uniqueCollection, collection);
+  return result;
+}
+
+function choose_no_repeat_number(collection) {
+  //在这里写入代码
+  var result = [];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    var resultLens = result.length;
+    var flag = false;
+    for (var j = 0; j < resultLens; j++) {
+      if (collection[i] === result[j]) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      result.push(collection[i]);
+    }
+  }
+  return result;
+}
+
+function getCount(uniqueCollection, collection) {
+  var result = [];
+  for (var i = 0, lens = uniqueCollection.length; i < lens; i++) {
+    var obj = {
+      key: uniqueCollection[i]
+    };
+    var count = 0;
+    for (var j = 0, collectionLens = collection.length; j < collectionLens; j++) {
+      if (collection[j] === uniqueCollection[i]) {
+        count++;
+      }
+    }
+    obj.count = count;
+    result.push(obj);
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
