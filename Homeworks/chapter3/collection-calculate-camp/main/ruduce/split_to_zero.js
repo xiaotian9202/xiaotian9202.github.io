d684dd8069b1a62b338275c78d9db3310685af32
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-29 13:44:03 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of split_to_zero.js

--- Homeworks/chapter3/collection-calculate-camp/main/ruduce/split_to_zero.js
+++ Homeworks/chapter3/collection-calculate-camp/main/ruduce/split_to_zero.js
@@ -2,6 +2,22 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var result = [];
+  result.push(number);
+  if (interval === 0.2) {
+    while (number !== 0) {
+      number -= interval;
+      number = Math.round(number * 100) / 100;
+      result.push(number);
+    }
+  } else if (interval === 0.3) {
+    while (number > 0) {
+      number -= interval;
+      number = Math.round(number * 100) / 100;
+      result.push(number);
+    }
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
