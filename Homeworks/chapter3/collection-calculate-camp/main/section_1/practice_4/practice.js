f86cc5c6e626fbfa22c338ea467e4270763149b6
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-29 22:28:53 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of section_1/practice_4/practice.js

--- Homeworks/chapter3/collection-calculate-camp/main/section_1/practice_4/practice.js
+++ Homeworks/chapter3/collection-calculate-camp/main/section_1/practice_4/practice.js
@@ -1,5 +1,15 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var result = [];
+  var lensA = collection_a.length;
+  for (var i = 0; i < lensA; i++) {
+    for (var j = 0, lens = object_b.value.length; j < lens; j++) {
+      if (collection_a[i].key === object_b.value[j]) {
+        result.push(collection_a[i].key);
+      }
+    }
+  }
+ return result;   
 }
 
 module.exports = collect_same_elements;
