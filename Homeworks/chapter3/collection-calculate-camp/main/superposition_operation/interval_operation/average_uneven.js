54fcec41f32982ac78c8684290a292855e28615d
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 19:41:06 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of interval_operation/average_uneven.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/interval_operation/average_uneven.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/interval_operation/average_uneven.js
@@ -3,6 +3,15 @@
 function average_uneven(collection) {
 
   //在这里写入代码
+  var sum = 0;
+  var count = 0;
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    if ((collection[i] & 1)) {
+      sum += collection[i];
+      count++;
+    }
+  }
+  return sum / count;
 }
 
 module.exports = average_uneven;
