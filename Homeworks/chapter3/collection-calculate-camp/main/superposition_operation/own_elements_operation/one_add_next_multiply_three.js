315f8b91e2b30b3909249ebaa79f714ac4693055
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 21:53:58 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: complete function of own_elements_operation/one_add_next_multiply_three.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,9 @@
 'use strict';
 function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+  var result = [];
+  for (var i = 0, lens = collection.length; i < lens - 1; i++) {
+    result.push((collection[i] + collection[i+1]) * 3);
+  }
+  return result;
 }
 module.exports = one_add_next_multiply_three;
