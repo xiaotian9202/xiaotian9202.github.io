387fb129c2965900859e54dafa58a0364e7845c9
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 19:38:38 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of interval_operation/amount_even.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/interval_operation/amount_even.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/interval_operation/amount_even.js
@@ -3,6 +3,13 @@
 function amount_even(collection) {
 
   //在这里写入代码
+  var sum = 0;
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    if (!(collection[i] & 1)) {
+      sum += collection[i];
+    }
+  }
+  return sum;
 }
 
 module.exports = amount_even;
