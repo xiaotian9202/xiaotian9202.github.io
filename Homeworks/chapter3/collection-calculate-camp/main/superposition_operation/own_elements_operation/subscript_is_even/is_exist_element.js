e14cf16cf87365c7a09af9ecc994460fe23f5cff
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 20:57:07 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: complete function of own_elements_operation/subscript_is_even/is_exist_element.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,27 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  var arr = getElementsWithEvenIndices(collection);
+  var bool = isFind(arr, element);
+  return bool;
 };
+
+function getElementsWithEvenIndices(collection) {
+  var arr = [];
+  for (var i = 0, lens = collection.length; i < lens; i += 2) {
+    arr.push(collection[i]);
+  }
+  return arr;
+}
+
+function isFind(collection, element) {
+  var bool = false;
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    if (element === collection[i]) {
+      bool = true;
+      break;
+    }
+  }
+  return bool;
+}
+
 module.exports = is_exist_element;
