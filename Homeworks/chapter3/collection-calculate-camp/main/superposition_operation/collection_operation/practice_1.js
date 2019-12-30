eeefa7ed6f727f10494cada4904cc1870a5f19c6
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 13:37:49 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of collection_operation/practice_1.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/collection_operation/practice_1.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/collection_operation/practice_1.js
@@ -3,6 +3,11 @@
 function hybrid_operation(collection) {
 
   //在这里写入代码
+  var sum = 0;
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    sum += (collection[i] * 3 + 2);
+  }
+  return sum;
 }
 
 module.exports = hybrid_operation;
