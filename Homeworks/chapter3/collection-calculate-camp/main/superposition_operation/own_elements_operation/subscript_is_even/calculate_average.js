f84749194e5014f96a0cc3f1795955ed92fcf991
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 19:52:18 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of own_elements_operation/subscript_is_even/calculate_average.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,14 @@
 'use strict';
 var calculate_average = function(collection){
+  var sum = 0;
+  var count = 0;
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    if (!(collection[i] & 1)) {
+      sum += collection[i];
+      count++;
+    }
+  }
+  return sum / count;
+}
 
-};
 module.exports = calculate_average;
