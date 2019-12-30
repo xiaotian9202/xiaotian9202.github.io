6812d6217cb6f8b81a816bda5d7cce363f9796aa
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 10:27:53 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of section_2/practice_2/practice.js

--- Homeworks/chapter3/collection-calculate-camp/main/section_2/practice_2/practice.js
+++ Homeworks/chapter3/collection-calculate-camp/main/section_2/practice_2/practice.js
@@ -1,5 +1,93 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var set = choose_no_repeat_element(collection);
+  var result = getCount(set, collection);
+  return result;
 }
 
+function choose_no_repeat_element(collection) {
+  var set = [];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    var strLens = collection[i].length;
+    if (strLens > 1) {
+      var obj = strToObj(collection[i], strLens);
+      collection[i] = obj;
+      if (!isFind(obj.name, set)) {
+        set.push(obj.name);
+      }
+    } else {
+      if (!isFind(collection[i], set)) {
+        set.push(collection[i]);
+      }
+    }
+  }
+  return set;
+}
+
+function isFind(element, set) {
+  var lens = set.length;
+  var flag = false;
+  for (var j = 0; j < lens; j++) {
+    if (element === set[j]) {
+      flag = true;
+      break;
+    }
+  }
+  return flag;
+}
+
+function strToObj(str, n) {
+  var obj = {
+    name: "",
+    count: ""
+  }
+  for (var j = 0; j < n; j++) {
+    if (str[j].charCodeAt() >= 97 && str[j].charCodeAt() <= 122
+      || str[j].charCodeAt() >= 65 && str[j].charCodeAt() <= 90) {
+      obj.name += str[j];
+    }
+    if (str[j].charCodeAt() >= 48 && str[j].charCodeAt() <= 57) {
+      obj.count += str[j];
+    }
+  }
+  obj.count = parseInt(obj.count);
+  return obj;
+}
+
+function getCount(set, collection) {
+  var result = [];
+  for (var i = 0, lens = set.length; i < lens; i++) {
+    var obj = {
+      key: set[i],
+      count: 0,
+    }
+    for (var j = 0, collectionLens = collection.length; j < collectionLens; j++) {
+      if (typeof(collection[j]) === "object") {
+        if (collection[j].name === set[i]) {
+          obj.count += collection[j].count;
+        }
+      } else {
+        if (collection[j] === set[i]) {
+          obj.count++;
+        }
+      }
+    }
+    result.push(obj);
+  }
+  return result;
+}
+
+// var collection = [
+//   // "a", "a", "a",
+//   // "e", "e", "e", "e", "e", "e", "e",
+//   // "h", "h", "h", "h", "h", "h", "h", "h", "h", "h", "h",
+//   // "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t",
+//   // "f", "f", "f", "f", "f", "f", "f", "f", "f",
+//   // "c", "c", "c", "c", "c", "c", "c", "c",
+//   // "g", "g", "g", "g", "g", "g", "g",
+//   // "b", "b", "b", "b", "b", "b",
+//   "d-5"
+// ];
+
+// console.log(count_same_elements(collection));
 module.exports = count_same_elements;
