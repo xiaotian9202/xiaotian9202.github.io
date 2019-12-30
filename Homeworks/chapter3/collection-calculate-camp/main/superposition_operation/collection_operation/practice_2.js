fd9007e56c5d4ffef0a68aa6e840d9492561e626
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 13:39:39 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of collection_operation/practice_2.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/collection_operation/practice_2.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/collection_operation/practice_2.js
@@ -3,6 +3,13 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var result = [];
+  for (var i = 0, lens = collection.length; i < lens; i++) {
+    if ((collection[i] & 1)) {
+      result.push(collection[i] * 3 + 2);
+    }
+  }
+  return result;
 }
 
 module.exports = hybrid_operation_to_uneven;
