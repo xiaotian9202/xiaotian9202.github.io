8cc64aaa83447abe08182755f58102635f44f3a5
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 20:06:48 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: complete function of own_elements_operation/subscript_is_even/calculate_median.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,20 @@
 'use strict';
 var calculate_median = function(collection){
-
+  var arr = [];
+  for (var i = 1, lens = collection.length; i < lens; i += 2) {
+    arr.push(collection[i]);
+  }
+  return getMedian(arr);
 };
+
+function getMedian(arr) {
+  var lens = arr.length;
+  var middle = Math.floor(lens / 2); 
+  if (!(lens & 1)) {
+    return Math.ceil((arr[middle] + arr[middle -1]) / 2);
+  } else {
+    return arr[middle];
+  }
+}
+
 module.exports = calculate_median;
