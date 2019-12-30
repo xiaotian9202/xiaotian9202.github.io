d62c504a082ac75b5455e670e91fa11ea377e316
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 22:08:46 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: complete function of own_elements_operation/rank_by_two_large_one_small.js

--- Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ Homeworks/chapter3/collection-calculate-camp/main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,17 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  collection = collection.sort(compare);
+  for (var i = 0, lens = collection.length; i + 3 < lens; i += 3) {
+    var temp = collection[i];
+    collection[i] = collection[i+1];
+    collection[i+1] = collection[i+2];
+    collection[i+2] = temp;
+  }
+  return collection;
 }
+
+function compare(a, b) {
+  return a - b;
+}
+
 module.exports = rank_by_two_large_one_small;
