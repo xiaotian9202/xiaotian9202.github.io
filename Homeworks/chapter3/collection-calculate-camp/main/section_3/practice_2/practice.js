12bbc11845ef2772026be398cb47cf1f1d42523f
xiaotian9202 <xiaotian9202@gmail.com>
2019-12-30 10:44:12 +0800

    [yanxiaotian] {feat}-{Chapter3-collection-calculate-camp}: Complete the function of section_3/practice_2/practice.js

--- Homeworks/chapter3/collection-calculate-camp/main/section_3/practice_2/practice.js
+++ Homeworks/chapter3/collection-calculate-camp/main/section_3/practice_2/practice.js
@@ -1,5 +1,23 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  for (var i = 0, lens = collection_a.length; i < lens; i++) {
+    if (isFind(collection_a[i].key, object_b.value)) {
+      collection_a[i].count -= Math.floor(collection_a[i].count / 3);
+    }
+  }
+  return collection_a;
+}
+
+function isFind(element, collection) {
+  var lens = collection.length;
+  var bool = false;
+  for (var j = 0; j < lens; j++) {
+    if (element === collection[j]) {
+      bool = true;
+      break;
+    }
+  }
+  return bool;
 }
 
 module.exports = create_updated_collection;
